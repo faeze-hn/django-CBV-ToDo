65528d8387c31d126eab8fb2e7ccfcec5667a406
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-09-21 15:50:35 +0330

    set the urls  for sccounts app and create login.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+from .views import LoginView,RegisterPage
+app_name = "accounts"
 
 urlpatterns = [
-    
+    path('login/',LoginView.as_view(), name= "login"),
+    path('logout/',LogoutView.as_view(), name= "logout"),
+    path('register/',RegisterPage.as_view(), name= "register")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 # Create your views here.
+class LoginView(LoginView):
+    template_name = "accounts/login.html"
+    fields = "username","password"
+    redirect_authentication_user = True
+    success_url = "/task-list/"
+
+    
+class RegisterPage(FormView):
+    pass
