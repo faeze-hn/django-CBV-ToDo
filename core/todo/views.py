d00ceeb204c4992a767ab058db71f1eab9d5e37c
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-09-21 16:45:56 +0330

    create taskview in views
    +list_task.html

--- core/todo/views.py
+++ core/todo/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from .models import Task
 # Create your views here.
 
 class TaskView(LoginRequiredMixin,ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "todo/list_task.html"
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
