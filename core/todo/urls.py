f4b49a771ef87e3a1c5cd275620f05256b56b9f8
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-09-21 17:09:01 +0330

    Update urls.py
    +update_task
    +complete_task
    +Delete_task

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import *
+from .views import (TaskList,TaskCreate,TaskDelete,TaskUpdate,TaskComplete)
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task_list"),
     path("create/",TaskCreate.as_view(), name="create_task"),
-    path("delete/",TaskDelete.as_view(), name="DeleteTask")
+    path("update/",TaskUpdate.as_view(), name="update_task"),
+    path("complete/",TaskComplete.as_view(), name="complete_task"),
+    path("delete/",TaskDelete.as_view(), name="Delete_task"),
 ]
