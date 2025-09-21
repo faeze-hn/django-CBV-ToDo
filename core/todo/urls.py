89d404542bbfee00e4292f7943a1f3c56117b694
Faeze Hoseini Nejad <rhwxhoseininejad@gmail.com>
2025-09-21 16:58:29 +0330

    add create task url and view

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task_list"),
+    path("create/",TaskCreate.as_view(), name="create_task")
 ]
--- core/todo/views.py
+++ core/todo/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
+
 # Create your views here.
 
-class TaskList(LoginRequiredMixin,ListView):
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
     template_name = "todo/list_task.html"
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ["title"]
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
